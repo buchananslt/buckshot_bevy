fe7f4c31f7c6b53d764f2ea03a41a019c65d0b42
buchananslt <brandanbuchanan@yahoo.com>
2023-11-28 19:43:05 -0800

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup_cam)
+    .run()
+}
+
+fn setup_cam(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera3dBundle::default());
+}
